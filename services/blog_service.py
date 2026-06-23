6728428136f52a482ef983e07bfe74d1f14b16ed
swati0602 <161737388+swati0602@users.noreply.github.com>
2026-06-23 20:58:40 +0530

    Add Streamlit integration for Google API key

--- services/blog_service.py
+++ services/blog_service.py
@@ -4,8 +4,11 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
+import streamlit as st
+
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash"
+    model="gemini-2.5-flash",
+    google_api_key=st.secrets["GEMINI_API_KEY"]
 )
 
 def generate_blog(topic):
