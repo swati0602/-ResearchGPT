5888b37b6f11a960642aff883a8db838b2c26c20
swati0602 <161737388+swati0602@users.noreply.github.com>
2026-06-23 21:03:29 +0530

    Refactor GEMINI_API_KEY retrieval in linkedin_service

--- services/linkedin_service.py
+++ services/linkedin_service.py
@@ -1,12 +1,19 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
+import streamlit as st
+import os
 
 load_dotenv()
 
+GEMINI_API_KEY = st.secrets.get(
+    "GEMINI_API_KEY",
+    os.getenv("GEMINI_API_KEY")
+)
+
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    google_api_key=st.secrets["GEMINI_API_KEY"]
+    google_api_key=GEMINI_API_KEY
 )
 
 def generate_linkedin(topic):
